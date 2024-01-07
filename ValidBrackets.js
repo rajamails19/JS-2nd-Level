069cd302383a62c685500a3addaf2fb97d9edd33
Raja V <rajamails19@gmail.com>
2024-01-06 19:33:11 -0800

    Create ValidBrackets.js

---
+++ ValidBrackets.js
@@ -0,0 +1,38 @@
+
+function isValid(s) 
+{
+  const stack = [], pairs = { '(': ')', '{': '}', '[': ']' };
+  
+  for (const c of s)
+    {
+    if (pairs[c]) stack.push(pairs[c]);
+    else if (stack.pop() !== c) return false;
+  }
+  return !stack.length;
+}
+
+/*
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+	1. Open brackets must be closed by the same type of brackets.
+	2. Open brackets must be closed in the correct order.
+	3. Every close bracket has a corresponding open bracket of the same type.
+ 
+Example 1:
+Input: s = "()"
+Output: true
+
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+
+Example 3:
+Input: s = "(]"
+Output: false
+
+
+*/
